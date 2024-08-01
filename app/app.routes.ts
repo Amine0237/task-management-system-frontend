04828b69186a2b0a066fcb1c9399c0a6cebabc93
Amine0237 <chadli.medamin@gmail.com>
2024-08-01 16:50:11 +0100

    finally setted up primeNG

--- app/app.routes.ts
+++ app/app.routes.ts
@@ -1,7 +1,7 @@
 import { Routes } from '@angular/router';
 
-import { Authority } from 'app/config/authority.constants';
-import { UserRouteAccessService } from 'app/core/auth/user-route-access.service';
+import { Authority } from '../app/config/authority.constants';
+import { UserRouteAccessService } from '../app/core/auth/user-route-access.service';
 import { errorRoute } from './layouts/error/error.route';
 
 import HomeComponent from './home/home.component';
--- app/shared/shared.module.ts
+++ app/shared/shared.module.ts
@@ -4,12 +4,15 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { AlertComponent } from './alert/alert.component';
 import { AlertErrorComponent } from './alert/alert-error.component';
+import { AutoCompleteModule } from 'primeng/autocomplete';
+import { ReactiveFormsModule } from '@angular/forms';
+import { ButtonModule } from 'primeng/button';
 
 /**
  * Application wide Module
  */
 @NgModule({
-  imports: [AlertComponent, AlertErrorComponent],
-  exports: [CommonModule, NgbModule, FontAwesomeModule, AlertComponent, AlertErrorComponent],
+  imports: [AlertComponent, AlertErrorComponent, AutoCompleteModule, ReactiveFormsModule, ButtonModule],
+  exports: [CommonModule, NgbModule, FontAwesomeModule, AlertComponent, AlertErrorComponent, ButtonModule, AutoCompleteModule],
 })
 export default class SharedModule {}
